d619cb2542352967cadbf732d1a0caf363c32f48
tomek makuch <tomek.makuch@gmail.com>
2022-02-02 10:55:57 +0100

    add several basic features : 2

--- code/core/settings.py
+++ code/core/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -183,6 +186,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
